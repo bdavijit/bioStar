32a1b44bbf3de9602ca2d3006318396876303a04
bdavijit <bdavijit@gmail.com>
2022-05-27 12:54:26 +0600

    Award

---
+++ src/components/Home/Award/Award.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Award = () => {
+    return (
+          <section className='WhereToBuy'>
+                <img
+                      src='https://ttesports.azureedge.net/files/Product_Review/2015100817284993-91.jpg'
+                      alt=''
+                />
+                <img
+                      src='https://img.healthcarepackaging.com/files/base/pmmi/all/image/2021/10/Tech_Awards_2021_logo.61575f955263d.png?ar=16%3A9&auto=format%2Ccompress&crop=focalpoint&fit=crop&fp-x=0.5&fp-y=0.5&h=639&q=70&w=1136'
+                      alt=''
+                />
+                <img
+                      src='https://dcassetcdn.com/design_img/2999837/664236/664236_16551236_2999837_dc3abe37_image.png'
+                      alt=''
+                />
+                <img
+                      src='https://employee-performance.com/blog/wp-content/uploads/2014/01/KvYzWn.jpg'
+                      alt=''
+                />
+                <img
+                      src='https://campustechnology.com/-/media/EDU/THEJournal/2020Editorial/20201201ctnewproductawardlogo300b.jpg'
+                      alt=''
+                />
+          </section>
+    );
+};
+
+export default Award;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 import BusinessSummary from '../Business Summary/BusinessSummary';
 import Parts from '../Parts/Parts';
 import Reviews from '../Reviews/Reviews';
+import Award from './Award/Award';
 import WheretoBuy from './Where to Buy/WheretoBuy';
 
 const Home = () => {
@@ -12,7 +13,9 @@ const Home = () => {
                 <BusinessSummary />
                 <Parts />
                 <h1 className='text-center text-2xl'>Where to Buy</h1>
-                <WheretoBuy/>
+                <WheretoBuy />
+                <h1 className='text-center text-2xl'>Award</h1>
+                <Award />
                 <Reviews />
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
                 voluptates assumenda explicabo modi enim ipsa ea facere
