486bbd35e6a303687abdcd6826ebebefbf0ec3e9
bdavijit <bdavijit@gmail.com>
2022-05-27 12:41:40 +0600

    whare to buy

---
+++ src/components/Home/Where to Buy/WheretoBuy.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './WheretoBuy.css'
+
+const WheretoBuy = () => {
+      return (
+            <section className='WhereToBuy'>
+                  <img
+                        src='https://upload.wikimedia.org/wikipedia/commons/5/5b/Daraz_Logo.png'
+                        alt=''
+                  />
+                  <img
+                        src='https://potheprobase.net/wp-content/uploads/2016/11/Pickaboo-Logo-01.jpg'
+                        alt=''
+                  />
+                  <img
+                        src='https://blog.logomyway.com/wp-content/uploads/2020/11/alibaba-logo-elements.jpg'
+                        alt=''
+                  />
+            </section>
+      );
+};
+
+export default WheretoBuy;
