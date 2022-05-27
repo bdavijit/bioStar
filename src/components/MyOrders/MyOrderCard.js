090915ab827e04362b88f86635bd105c50f7d54a
bdavijit <bdavijit@gmail.com>
2022-05-27 23:04:25 +0600

    delete order

--- src/components/MyOrders/MyOrderCard.js
+++ src/components/MyOrders/MyOrderCard.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
 const MyOrderCard = (props) => {
-    const { pName, pImage, pPrice, quintity, paid } = props.Orders;
+    const { _id, pName, pImage, pPrice, quintity, paid } = props.Orders;
+              const DeleteOrder = (id) => {
+                    const proceed = window.confirm(
+                          'Are you sure you want to delete?'
+                    );
+                    if (proceed) {
+                          const url = `http://localhost:5001/Order/${id}`;
+                          fetch(url, {
+                                method: 'DELETE',
+                          })
+                                .then((res) => res.json())
+                                .then((data) => {
+                                      if (data.deletedCount > 0) {
+                                            const remaining =
+                                                  props.orders.filter(
+                                                        (order) =>
+                                                              order._id !== id
+                                                  );
+                                            props.Setorders(remaining);
+                                      }
+                                      alert('ok');
+                                });
+                    }
+              };
+
     return (
           <>
                 <img src={pImage} alt='' />
@@ -9,6 +33,9 @@ const MyOrderCard = (props) => {
                 <p>Price : {pPrice}</p>
                 <p>quintity: {quintity}</p>
                 {!paid ? <button className='btn'>payNow</button> : 'paid'}
+                <button className='btn' onClick={() => DeleteOrder(_id)}>
+                      Delete
+                </button>
           </>
     );
 };
--- src/components/MyOrders/MyOrders.js
+++ src/components/MyOrders/MyOrders.js
@@ -4,14 +4,19 @@ import useFindMyOrder from '../../hook/useFindMyOrder';
 import MyOrderCard from './MyOrderCard';
 
 const MyOrders = () => {
-const { email } = useParams();
+      const { email } = useParams();
       const [myOrders, setmyOrders] = useFindMyOrder(email);
       console.log(myOrders);
 
       return (
             <div>
                   {myOrders?.map((Orders) => (
-                        <MyOrderCard Orders={Orders}  key={Orders._id}/>
+                        <MyOrderCard
+                              Orders={Orders}
+                              key={Orders._id}
+                              orders={myOrders}
+                              Setorders={setmyOrders}
+                        />
                   ))}
             </div>
       );
