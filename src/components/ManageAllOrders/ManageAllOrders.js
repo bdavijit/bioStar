7c273ba3024c25144581a361c921fa07fbfce321
bdavijit <bdavijit@gmail.com>
2022-05-30 18:42:37 +0600

    update performance

--- src/components/ManageAllOrders/ManageAllOrders.js
+++ src/components/ManageAllOrders/ManageAllOrders.js
@@ -1,23 +1,46 @@
 import React from 'react';
+import useFindAdmin from '../../hook/usefindAdmin';
+import useFirebase from '../../hook/useFirebase';
 import useOrder from '../../hook/useOrder';
+import Login2 from '../Login/Login';
 import ManageAllOrdersCard from './ManageAllOrdersCard';
 
 const ManageAllOrders = () => {
       const [allOrder, setallOrder, rerenderOrder, setrerenderOrder] =
             useOrder();
+                  const { user } = useFirebase();
+
+                  const [myUsers, setmyUsers] = useFindAdmin(user?.email);
       return (
-            <section>
-                  {allOrder?.map((Order) => (
-                        <ManageAllOrdersCard
-                              key={Order._id}
-                              Order={Order}
-                              orders={allOrder}
-                              Setorders={setallOrder}
-                              rerenderOrder={rerenderOrder}
-                              setrerenderOrder={setrerenderOrder}
-                        ></ManageAllOrdersCard>
-                  ))}
-            </section>
+            <>
+                  {user && myUsers?.role ? (
+                        <section>
+                              {allOrder?.map((Order) => (
+                                    <ManageAllOrdersCard
+                                          key={Order._id}
+                                          Order={Order}
+                                          orders={allOrder}
+                                          Setorders={setallOrder}
+                                          rerenderOrder={rerenderOrder}
+                                          setrerenderOrder={setrerenderOrder}
+                                    ></ManageAllOrdersCard>
+                              ))}
+                        </section>
+                  ) : (
+                        <>
+                              {myUsers?.role === undefined ? (
+                                    <>
+                                          <h1 className='text-center'>
+                                                Please login as a Admin
+                                          </h1>
+                                          <Login2 />
+                                    </>
+                              ) : (
+                                    <h1>test</h1>
+                              )}
+                        </>
+                  )}
+            </>
       );
 };
 
--- src/components/ManageAllOrders/ManageAllOrdersCard.js
+++ src/components/ManageAllOrders/ManageAllOrdersCard.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import useFindAdmin from '../../hook/usefindAdmin';
-import useFirebase from '../../hook/useFirebase';
-import Login2 from '../Login/Login';
 import './ManageAllOrder.css';
 
 const ManageAllOrdersCard = (props) => {
-      const { user } = useFirebase();
 
-      const [myUsers, setmyUsers] = useFindAdmin(user?.email);
 
-      console.log(user?.email);
-      console.log(myUsers?.role);
       const {
             _id,
             pId,
@@ -76,69 +69,44 @@ const ManageAllOrdersCard = (props) => {
             }
       };
       return (
-            <>
-                  {user && myUsers?.role ? (
-                        <section className='ManageAllOrderBox'>
-                              <div className='ManageAllSec'>
-                                    <img src={pImage} alt='' />
-                              </div>
-                              <div className=''>
-                                    <p className='text-center'>{pName}</p>
-                                    <p className='text-center'>{pPrice}</p>
-                                    <p className='text-center'>{status}</p>
-                              </div>
-                              <div className='ManageAllSec'>
-                                    {status === 'unpaid' ? (
-                                          <>
-                                                <button
-                                                      className='btn'
-                                                      onClick={() =>
-                                                            OrderStateChange(
-                                                                  'paid'
-                                                            )
-                                                      }
-                                                >
-                                                      paid order
-                                                </button>
-                                                <button
-                                                      className='btn m-2 '
-                                                      onClick={() =>
-                                                            DeleteOrder(_id)
-                                                      }
-                                                >
-                                                      Delete
-                                                </button>
-                                          </>
-                                    ) : (
-                                          <button
-                                                className='btn'
-                                                onClick={() =>
-                                                      OrderStateChange(
-                                                            'shipped'
-                                                      )
-                                                }
-                                                disabled={status === 'shipped'}
-                                          >
-                                                shipped
-                                          </button>
-                                    )}
-                              </div>
-                        </section>
-                  ) : (
-                        <>
-                              {myUsers?.role === undefined ? (
-                                    <>
-                                          <h1 className='text-center'>
-                                                Please login as a Admin
-                                          </h1>
-                                          <Login2 />
-                                    </>
-                              ) : (
-                                    <h1>test</h1>
-                              )}
-                        </>
-                  )}
-            </>
+            <section className='ManageAllOrderBox'>
+                  <div className='ManageAllSec'>
+                        <img src={pImage} alt='' />
+                  </div>
+                  <div className=''>
+                        <p className='text-center'>{pName}</p>
+                        <p className='text-center'>{pPrice}</p>
+                        <p className='text-center'>{status}</p>
+                  </div>
+                  <div className='ManageAllSec'>
+                        {status === 'unpaid' ? (
+                              <>
+                                    <button
+                                          className='btn'
+                                          onClick={() =>
+                                                OrderStateChange('paid')
+                                          }
+                                    >
+                                          paid order
+                                    </button>
+                                    <button
+                                          className='btn m-2 '
+                                          onClick={() => DeleteOrder(_id)}
+                                    >
+                                          Delete
+                                    </button>
+                              </>
+                        ) : (
+                              <button
+                                    className='btn'
+                                    onClick={() => OrderStateChange('shipped')}
+                                    disabled={status === 'shipped'}
+                              >
+                                    shipped
+                              </button>
+                        )}
+                  </div>
+            </section>
       );
 };
 
